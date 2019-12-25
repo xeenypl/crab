96d7e94ad9f684bb2ecf13351fedd5baa6bb6f9b
xeeny <xeeny@wp.pl>
2019-12-25 16:45:36 +0100

    added core mechanics

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,22 @@ use scraper;
 use std::io;
 use std::io::Read;
 
+fn get_content(name: &str) -> String {
+    if name == "stdin" {
+        let mut buf = String::new();
+        io::stdin()
+            .lock()
+            .read_to_string(&mut buf)
+            .expect(&ansi_term::Color::Red.paint(format!("problen with open {}.", name)));
+        return buf;
+    } else {
+        return reqwest::get(name)
+            .expect(&format!("felie reqwest to {}", name))
+            .text()
+            .unwrap();
+    }
+}
+
 fn main() {
     let matches = App::new("crab - cli web scraper")
         .version("0.1.0")
@@ -83,4 +99,40 @@ fn main() {
                 ),
         )
         .get_matches();
+
+    let soures: String = matches.value_of("URL").unwrap().to_string();
+    let width: String = matches.value_of("width").unwrap_or("80").to_owned();
+    let comment: String = get_content(&soures);
+
+    if let Some(matches) = matches.subcommand_matches("get") {
+        let selector: String = matches.value_of("SELECTOR").unwrap().to_string();
+        let document = scraper::Html::parse_document(&comment);
+        let selector = scraper::Selector::parse(&selector)
+            .expect(&ansi_term::Color::Red.paint(&format!("wrong selector:\n\t {}", &selector)));
+        for elem in document.select(&selector) {
+            if matches.is_present("attribute") {
+                if !matches.is_present("show-dom") {
+                    let attr = matches.value_of("attribute").unwrap();
+                    println!("{}", elem.value().attr(attr).unwrap_or(""));
+                } else {
+                    unimplemented!();
+                }
+            } else {
+                if !matches.is_present("show-dom") {
+                    println!("{}", elem.text().collect::<Vec<_>>().join(""));
+                } else {
+                    unimplemented!();
+                }
+            }
+        }
+    } else {
+        if !matches.is_present("show-dom") {
+            println!(
+                "{}",
+                html2text::from_read(&mut comment.as_bytes(), width.parse().unwrap())
+            );
+        } else {
+            unimplemented!();
+        }
+    }
 }
